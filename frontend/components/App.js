934d8c1e8057b9536a6a555f439faee7dddb3741
Mirwes Hakimi <mirwes215@gmail.com>
2025-02-17 21:10:56 -0800

    added constructor to the app js, also added the state to add a new to do to the list. on the form component added the constructor and the state to update the input field. Finally, added the handleSubmit to submit the form

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,10 +1,38 @@
-import React from 'react'
+import React from 'react';
+import Form from './Form';
+import Todo from './Todo';
+import TodoList from './TodoList';
+import { date } from 'yup';
+
+
 
 export default class App extends React.Component {
+  constructor (){
+    super();
+    this.state = {
+    todos: [
+      {
+        name: 'Organize the garage',
+        id: Math.random(),
+        compeleted: false
+      }
+    ]
+    }
+  }
+  addToDo = (addName) => {
+    const newToDo = {
+      name: addName,
+      id: Date.now(),
+      compeleted: false
+    }
+
+    this.setState({todos: [...this.state.todos, newToDo]})
+  }
   render() {
     return (
       <div>
-        Todo App
+        <Form addToDo = {this.addToDo}/>
+        <TodoList todos={this.todos} />
       </div>
     )
   }
--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -1,10 +1,43 @@
 import React from 'react'
 
 export default class Form extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+     name: "",
+
+    }
+  }
+  handleChangeInput = (event) => {
+    
+   this.setState({
+    name: event.target.value,
+   })
+  }
+
+  handleSubmit = (event) => {
+   event.preventDefault();
+   /// call addToDo passed from app.js to add the new to do
+   this.props.addToDo(this.state.name)
+   /// clear the input feild
+   this.setState({
+    name:"",
+   })
+  }
+
   render() {
     return (
       <div>
-        Form
+       <form onSubmit={this.handleSubmit}>
+        <input 
+        type='text'
+        placeholder='Type your to dos' 
+        value={this.state.name} /// bind input feild to the state
+        onChange={this.handleChangeInput} /// update the state when input change
+        />
+        <button type='submit'>Submit</button>
+      
+       </form>
       </div>
     )
   }
